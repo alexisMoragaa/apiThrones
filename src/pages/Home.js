a6c9d1ab1e579af935e0ffdd4e19af3a37824104
Alexis Moraga <alexis.moraga.gallardo@gmail.com>
2021-05-03 23:19:40 -0400

    1.- Se añade un componente app para gestionar las redirecciones con react router dom 2.- Se añade un componente layout dentro del cual rendereamos el contenido de la pagina 3.- creamos un componente nav 4.- creamos pagina home 5.- Se instala meterial-ui/icons

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+function Home (){
+    return(
+        <React.Fragment>
+            <h1>Esta es la pagina de home</h1>
+        </React.Fragment>
+    )
+}
+
+export default Home
---
+++ src/pages/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+
+function NotFound(){
+    return(
+        <React.Fragment>
+            <h1>Esta es la pagina de not found #404</h1>
+        </React.Fragment>
+    )
+}
+
+export default NotFound
